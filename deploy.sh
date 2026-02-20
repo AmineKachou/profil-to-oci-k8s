2990d14c2e809a3439b19df86df8c264d7c368ba
uvsq22308195 <54586851+safa-lazrek@users.noreply.github.com>
2026-02-20 13:10:41 +0100

    update
    Removed service and deployment manifests from deployment script.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ kubectl apply -f output/k8s/namespace.yaml
 
 # Appliquer le reste des manifests
 kubectl apply -f output/k8s/networkpolicy.yaml
-kubectl apply -f output/k8s/service.yaml
-kubectl apply -f output/k8s/deployment.yaml
+
