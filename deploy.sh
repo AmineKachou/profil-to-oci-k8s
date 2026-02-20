b5536c74f46147bfe58b93e38c12dc5930c3c924
uvsq22308195 <54586851+safa-lazrek@users.noreply.github.com>
2026-02-20 13:11:08 +0100

    update2

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ kubectl apply -f output/k8s/namespace.yaml
 
 # Appliquer le reste des manifests
 kubectl apply -f output/k8s/networkpolicy.yaml
+kubectl apply -f output/k8s/service.yaml
+kubectl apply -f output/k8s/deployment.yaml
 
